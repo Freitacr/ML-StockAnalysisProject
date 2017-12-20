45434e2e003fe298f66837feae4f1766e00b0def
colton <colton@Rex>
2017-12-19 22:46:16 -0800

    First Successful Data Retrieval

--- src/StockDataDownloading/HTTPUtils/HTTPUtils.py
+++ src/StockDataDownloading/HTTPUtils/HTTPUtils.py
@@ -1,14 +1,25 @@
 '''
 Created on Dec 19, 2017
 
-@author: colton
+@author: Colton Freitas
 '''
 import urllib.request as ureq
+from GeneralUtils.EPrint import eprint
+from urllib.error import HTTPError
 
 
-def openURL(url):
+def openURL(url, cookie = None):
     '''Opens the URL specified with basic error reporting'''
-    hres = ureq.urlopen(url)
+    opener = ureq.build_opener()
+    if not cookie == None:
+        print(cookie)
+        opener.addheaders.append( ('Cookie', cookie))
+    hres = None
+    try:
+        hres = opener.open(url)
+    except HTTPError as e:
+        eprint(str(e))
+        return [False, str(e)]
     if not str(hres.getcode())[0] == '2':
         return [False, hres.getcode()]
     else:
