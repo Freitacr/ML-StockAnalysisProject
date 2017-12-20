06a0741a28e568f9bf0993c077ff794a8e6edf2c
colton <colton@Rex>
2017-12-19 16:53:48 -0800

    Project Reorganization and First Comments

---
+++ src/DailyStockDataDownloader.py
@@ -0,0 +1,10 @@
+'''
+Created on Dec 19, 2017
+
+@author: colton
+'''
+import StockDataDownloading
+
+
+if __name__ == '__main__':
+    print("HelloWorld")
---
+++ src/StockDataDownloading/DownloaderYahoo.py
@@ -0,0 +1,25 @@
+'''
+Created on Dec 19, 2017
+
+@author: colton
+
+@return: 
+
+Indented Purpose: Given a list of stock tickers to download, download the historical
+    data for each of the stock tickers. If the ticker is not valid, two more attempts (on a timeout)
+    should be made to download data for it. In the event that the ticker still has an error,
+    it should be returned within the errored_tickers array.
+Intented Purpose: Given a list of stock tickers to download, download the historic data
+    for each of the stock tickers. The data and any errored tickers should be returned as separate lists
+    The data should originate from Yahoo!, and should deal with Yahoo!'s download links.
+'''
+
+# TODO: Find information regarding whether this structure will work when importing the functions
+
+
+#def getHistoricalData(ticker_list, max_number_of_days=-1)
+#Docstring discussing the arguments and the purpose of the function
+
+#def __getDataForTicker(ticker, max_number_of_days)
+#Docstring noting its relation to getHIstoricalData
+
