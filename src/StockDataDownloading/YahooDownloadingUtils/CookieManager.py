3ab65fe906a58fae5d0306c571e7298037299342
colton <colton@Rex>
2017-12-19 17:01:26 -0800

    Blank Class Creation

---
+++ src/StockDataDownloading/YahooDownloadingUtils/CookieManager.py
@@ -0,0 +1,17 @@
+'''
+Created on Dec 19, 2017
+
+@author: colton
+'''
+#TODO: Fill out class structure, adding methods for cookie and crumb obtainment, and storage for those two pieces of info respectively
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
