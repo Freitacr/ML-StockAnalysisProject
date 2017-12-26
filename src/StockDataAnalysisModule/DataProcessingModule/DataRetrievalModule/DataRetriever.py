7767c62c4d2a9f79bee42a25555e0426e2697b8b
Colton Freitas <colton@Rex>
2017-12-26 09:11:25 -0800

    Prediction Framework Update
    This is largely just a push of the attempts to design a solid foundation
    for further ML predictions, however it has one testing method for a ML
    Prediction already, and it has produced interesting results on the
    training data it has been given. Admittedly, the high score is likely
    just training data bias, but its enough to warrant further testing.

---
+++ src/StockDataAnalysisModule/DataProcessingModule/DataRetrievalModule/DataRetriever.py
@@ -0,0 +1,40 @@
+'''
+Created on Dec 24, 2017
+
+@author: Colton Freitas
+'''
+
+from GeneralUtils.StockDataMYSQLManagement.MSYQLDataManipulator import MYSQLDataManipulator
+from asyncio.futures import InvalidStateError
+
+class DataRetriever:
+    
+    def __init__ (self, login_credentials, column_list):
+        self.data_man = MYSQLDataManipulator(login_credentials[0], login_credentials[1], login_credentials[2], login_credentials[3])
+        self.column_list = column_list
+        self.__setupStockSourceList()
+        self.__retrieveData()
+        self.data_man.close(commit = False)
+    def __setupStockSourceList(self):
+        #Right now this is going to be a lot more temporary, just using whatever source has actual data on the stock
+        #The sources are also going to be in order of preference of use (Yahoo, then Google right now. Even though Google has no data
+        #Downloading yet)
+        source_availability = self.data_man.select_from_table('stock_list', ['ticker', 'yahoo', 'google'])
+        tickers = [x[0] for x in source_availability]
+        yahoo_availability = [x[1] for x in source_availability]
+        google_availability = [x[2] for x in source_availability]
+        self.data_sources = []
+        for index in range(len(tickers)):
+            if yahoo_availability[index]:
+                self.data_sources.extend([(tickers[index], 'yahoo')])
+            elif google_availability[index]:
+                self.data_sources.extend([(tickers[index], 'google')])
+            else:
+                raise InvalidStateError("%s has no data, but exists in the database. This is bad." % tickers[index])
+    def __retrieveData(self):
+        self.retrieved_data = []
+        for ticker, source in self.data_sources:
+            self.retrieved_data.extend([[ticker, self.data_man.select_from_table("%s_%s_data" % (ticker, source), self.column_list)]])
+        
+    def getData(self):
+        return self.retrieved_data
