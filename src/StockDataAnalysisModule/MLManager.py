93222382416457b0ee8323f623ed9bd623cadcd6
Colton Freitas <colton@Rex>
2017-12-26 22:51:06 -0800

    First Accuracy Models
    Adds in a method to get an overall cross-fold based accuracy for two of
    the machine learning techniques included in MLModels (NaiveBayes and
    DecisionTree)

--- src/StockDataAnalysisModule/MLManager.py
+++ src/StockDataAnalysisModule/MLManager.py
@@ -5,13 +5,8 @@ Created on Dec 25, 2017
 '''
 
 from .DataProcessingModule.DataProcessor import DataProcessor
-from .MLModels import DecisionTree, NaiveBayes
-
-
-#TODO: Create method for Decision Tree training
-#TODO: Create class for info file managing (should be similar to configparser)
-
-
+from .MLModels import DecisionTree, NaiveBayes, KNN
+from configparser import NoSectionError, NoOptionError
 
 class MLManager:
     
@@ -21,49 +16,130 @@ class MLManager:
         
     def basicMovementsTraining(self, ML_Dir, ml_model = "naive_bayes"):
         if not ml_model.lower() in self.valid_models:
-            raise ValueError("Invalid model %s\n Valid Models: %s" % (ml_model, str(self.valid_models)))
-        
+            raise ValueError("Invalid model %s\nValid Models: %s" % (ml_model, str(self.valid_models)))
+        info_man = InfoManager()
         ticker_data = self.data_handler.getMovementDirections(['adj_close', 'opening_price', 'close_price'], max_number_of_days_per_example = 30)
         stored_tickers = ticker_data.getContained_tickers()
-        model_info_file = open(ML_Dir + "/%s_movement_direction_models.mlinf" % (ml_model), 'w')
-        bigX, bigY = [[], []]
+        if ml_model.lower() == 'naive_bayes':
+            cross_model = NaiveBayes.NaiveBayes(assume_all_features_known=False)
+            ret_model = NaiveBayes.NaiveBayes(assume_all_features_known=False)
+        elif ml_model.lower() == 'decision_tree':
+            cross_model = DecisionTree.DecisionTree()
+            ret_model = DecisionTree.DecisionTree()
+        elif ml_model.lower() == 'knn':
+            cross_model = KNN.KNN()
+            ret_model = KNN.KNN()
+        X,Y = [[], []]
         for ticker in stored_tickers:
-            X,Y = ticker_data.getTickerData(ticker)
-            if ml_model.lower() == 'naive_bayes':
-                ticker_bayes = NaiveBayes.NaiveBayes(assume_all_features_known=False)
-            elif ml_model.lower() == 'decision_tree':
-                ticker_bayes = DecisionTree.DecisionTree()
-            ticker_bayes.train(X,Y)
-            print("For %s" % ticker, self.__naiveBayesAccuracyTesting(ticker_bayes, ticker_data))
-            bigX.extend(X)
-            bigY.extend(Y)
-        if ml_model == 'naive_bayes':
-            big_bayes = NaiveBayes.NaiveBayes(assume_all_features_known=False)
-        elif ml_model == 'decision_tree':
-            big_bayes = DecisionTree.DecisionTree()
-        big_bayes.train(bigX, bigY)
-        print("For overall:", self.__naiveBayesAccuracyTesting(big_bayes, ticker_data))
-        model_info_file.close()
+            data = ticker_data.getTickerData(ticker)
+            X.extend(data[0])
+            Y.extend(data[1])
+        print("Beginning Training...")
+        ret_model.train(X, Y)
+        print("Training completed.")
         
-    def __naiveBayesAccuracyTesting(self, bayes_model, ticker_data):
+        info_man.addSectionWithOptions("overall", self.__overallCrossfoldAccuracy(cross_model, ticker_data))
+        #print("For overall:", self.__naiveBayesAccuracyTesting(big_bayes, ticker_data))
+        info_man.writeFile(ML_Dir + "/%s_movement_direction_models.mlinf" % (ml_model))
+        ret_model.store(ML_Dir + "/%s_movement_direction_models.mlmdl" % (ml_model))
+        
+    def __overallCrossfoldAccuracy(self, model, ticker_data):
         ret_dict = {}
-        bigX = []
-        bigY = []
-        for ticker in ticker_data.getContained_tickers():
-            X,Y = ticker_data.getTickerData(ticker)
+        stored_tickers = ticker_data.getContained_tickers()
+        for ticker in stored_tickers:
             correct = 0
-            for example_index in range(len(X)):
-                if str(bayes_model.predict(X[example_index])) == str(Y[example_index]):
+            testX, testY = ticker_data.getTickerData(ticker)
+            X, Y = [[], []]
+            for other_ticker in stored_tickers:
+                if other_ticker == ticker:
+                    continue
+                oX, oY = ticker_data.getTickerData(other_ticker)
+                X.extend(oX)
+                Y.extend(oY)
+            model.train(X,Y)
+            for ex_index in range(len(testX)):
+                if str(model.predict(testX[ex_index])) == str(testY[ex_index]):
                     correct += 1
-            ret_dict[ticker] = float(correct) / len(X) #I cast to float because I'm used to Java, and don't trust integer division to be floating point return
-            bigX.extend(X)
-            bigY.extend(Y)
-        correct = 0
-        for example_index in range(len(bigX)):
-            if str(bayes_model.predict(bigX[example_index])) == str(bigY[example_index]):
-                correct += 1
-        ret_dict['overall'] = float(correct) / len(bigX)
+            ret_dict [ticker] = correct / len(testX)
         return ret_dict
+    
+class InfoManager:
+    
+    def __init__(self):
+        self.section_dict = {}
+    
+    def addSection(self, section_name):
+        if not type(section_name) == type(""):
+            raise ValueError("Section name was not of type %s, it was of type %s" % (str(type("")), str(type(section_name))))
+        self.section_dict[section_name] = {}
+    
+    def addOption(self, section_name, option_name, value):
+        try:
+            self.section_dict[section_name][option_name] = value
+        except KeyError:
+            raise NoSectionError("No section named %s" % section_name)
+    
+    def writeFile(self, file_path):
+        file = None
+        try:
+            file = open(file_path, 'w')
+        except FileNotFoundError as e:
+            raise e
+        for key, value in self.section_dict.items():
+            file.write("%s\n" % key)
+            for sub_key, sub_value in value.items():
+                file.write("\t%s:%s\n" % (sub_key, (sub_value)))
+        file.close()
+    
+    def readFile(self, file_path):
+        self.section_dict = {}
+        file = None
+        current_section = None
+        try:
+            file = open(file_path, 'r')
+        except FileNotFoundError as e:
+            raise e
+        for line in file:
+            if line[0] == "\t":
+                linesplit = line.strip().split(":")
+                self.addOption(current_section, linesplit[0], linesplit[1])
+            else:
+                self.addSection(line.strip())
+                current_section = line.strip()
+        file.close()
+    
+    def getOption(self, section, option):
+        try:
+            section = self.section_dict[section]
+        except KeyError:
+            raise NoSectionError("No section named %s" % section)
+        try:
+            return section[option]
+        except KeyError:
+            raise NoOptionError("No option named %s" % option)
+    
+    def getSectionList(self):
+        return self.section_dict.keys()
+    
+    def getOptionList(self, section):
+        try:
+            return self.section_dict[section].keys()
+        except KeyError:
+            raise NoSectionError("No section named %s" % section)
+        
+    def addSectionWithOptions(self, section_name, options):
+        if not type(section_name) == type(""):
+            raise ValueError("Section name was not of type %s, it was of type %s" % (str(type("")), str(type(section_name))))
+        self.section_dict[section_name] = options
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
