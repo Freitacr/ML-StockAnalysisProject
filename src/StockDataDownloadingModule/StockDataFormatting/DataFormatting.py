19b33e840a98d0e73c30492ff1dcb00f8d4e5c36
Colton Freitas <colton@Rex>
2018-01-03 21:18:36 -0800

    Documentation Update
    Added fuller documentation to the StockDataDownloadingModule

--- src/StockDataDownloadingModule/StockDataFormatting/DataFormatting.py
+++ src/StockDataDownloadingModule/StockDataFormatting/DataFormatting.py
@@ -9,13 +9,16 @@ from .YahooDataFormatting import YahooDataFormatting
 class DataFormatter:
     
     def __init__(self, login_credentials, ticker_list):
+        '''Initialization method
+        @param ticker_list: List of stock tickers to obtain and format data for
+        @param login_credentials: Login credentials for the MySQL Server 
+        '''
         self.login_credentials = login_credentials
         self.ticker_list = ticker_list
         
     def getData(self):
-        '''
-        returns ret_data which is a formatted array with:
-        [[Yahoo,[data[]],Google,[data[]]]]
+        '''Obtain and format data on all tickers from self.ticker_list
+        @return List formatted as such [ [datasource1, [ [ticker, [day1data, day2data...] ], [ticker2 ....] ] ], [datasourceN ... ] ]
         '''
         ret_data = []
         yah_formatter = YahooDataFormatting(self.ticker_list, self.login_credentials)
